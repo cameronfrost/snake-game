b056eb16011a952e3b328f6fdbeed0450662e803
Cameron <c@cameronfrost.com>
2021-01-13 17:42:25 -0600

    Styling and keycodes

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ function createGrid() {
   console.log(squares);
 }
 createGrid();
+
+currentSnake.forEach(index => squares[index].classList.add("snake"));
+
+function move() {
+  const tail = currentSnake.pop();
+  squares[tail].classList.remove("snake");
+  currentSnake.unshift(currentSnake[0] + direction);
+  squares[currentSnake[0]].classList.add("snake");
+}
+move();
+
+let timerId = setInterval(move, 1000);
+
+// 39 is right arrow
+// 38 is for the up arrow
+// 37 is for the left arrow
+// 40 is for the down arrow
+
+function control(e) {
+  if (e.keyCode === 39) {
+    console.log("right pressed");
+  } else if (e.keyCode === 38) {
+    console.log("up pressed");
+  } else if (e.keyCode === 37) {
+    console.log("left pressed");
+  } else if (e.keyCode === 40) {
+    console.log("down pressed");
+  }
+}
